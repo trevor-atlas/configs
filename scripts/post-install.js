05cfea722aa43ffc8d47f03d1278a2ecb741d00b
Trevor Atlas <me@trevoratlas.com>
2020-03-09 00:13:20 -0400

    db

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -7,6 +7,7 @@ const links = require('./links.json');
 
 (async () => {
 	const projectDir = process.env.INIT_CWD || path.resolve("../../", __dirname);
+	console.log('projectdir', projectDir)
 	for (let link of links) {
 		await symLink(`./${link}`, join(projectDir, link));
 	}
--- scripts/pre-uninstall.js
+++ scripts/pre-uninstall.js
@@ -8,7 +8,7 @@ const links = require('./links.json');
 (async () => {
 	const projectDir = process.env.INIT_CWD || path.resolve("../../", __dirname);
 	console.log('projectdir', projectDir)
-	for (let link in links) {
+	for (let link of links) {
 		await unlinkAsync(`./${link}`, join(projectDir, link));
 	}
 })();
